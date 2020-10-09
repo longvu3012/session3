16368ec6e958443c08d082d5be0ed8c64e3af845
longvu3012 <72186778+longvu3012@users.noreply.github.com>
2020-10-09 15:32:05 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// -------------------------------STUDY--------------------------------
+// ===========Ex1==========:
+
+// Examples 1:
+// Swap trực tiếp:
+// let a = 5;
+// let b = 6;
+// [a , b] = [b ,a ];
+// console.log(`${a}, ${b}`);
+
+// Examples 2:
+// Swap bằng cách sử dụng thuật toán hoán đổi vị trí:
+// let a = 5;
+// let b = 6;
+// let giatri;
+// giatri = a;
+// a= b;
+// b = giatri;
+// console.log(a,b);
+
+// ===========Ex2==========:
+
+// const s = "Hello beauty there";
+// const a = Array[s];
+// console.log(a);
+// s.split = String("", 2);
+// console.log(a);
+
+const a = [4, 5 , 7, -8];
+console.log(...a);
